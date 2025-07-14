24d2803dc898d08fa992ca5bf838a1fa7670d3ad
root <root@srv886970.hstgr.cloud>
2025-07-14 04:09:43 +0000

    bd config

--- mi-backend/config/db.js
+++ mi-backend/config/db.js
@@ -1,8 +1,8 @@
 const mysql = require('mysql2/promise');
 
 const db = mysql.createPool({
-  host: 'localhost',
-  user: 'root',
+  host: '127.0.0.1',
+  user: 'usuario',
   password: 'root',
   database: 'biblioteca1'
 });
